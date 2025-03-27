7bf53b25386bdbfef7d027ef3c05a87be14afe56
John Curtis <john@beany.com>
2025-03-28 12:12:43 +1300

    Fixes for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from '@vuejs/vite-plugin-vue-devtools'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ear-training-app/',
   plugins: [
     vue(),
     vueDevTools(),
