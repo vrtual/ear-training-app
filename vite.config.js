57c808b966eeaa65b4bbebd421699dc5b21c6837
John Curtis <john@beany.com>
2025-03-28 12:40:19 +1300

    Including app name in path

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ear-training-app/',
   plugins: [
     vue(),
     vueDevTools(),
